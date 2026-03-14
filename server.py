5c7cf3cfed2192f4345e4ca690c487419871a111
travii2008 <travisramlall12@gmail.com>
2026-03-14 10:14:38 -0400

    Fix port binding for Render

--- server.py
+++ server.py
@@ -305,5 +305,5 @@ def pdf_to_jpg():
     except Exception as e: return jsonify({"error":str(e)}), 500
 
 if __name__=="__main__":
-    port=int(os.environ.get("PORT",5000))
+    port=int(os.environ.get("PORT",10000))
     app.run(host="0.0.0.0", port=port)
